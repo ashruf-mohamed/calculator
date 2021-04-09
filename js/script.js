ce9f838954593c741f0c8fa24af434c1c912eef8
Ashruf mohammed <72321696+ashruf-mohamed@users.noreply.github.com>
2021-04-09 19:02:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,143 +1,136 @@
+/*global console, alert, document*/
 
+var history1 = document.getElementById("history");
 var current = document.getElementById("current");
-var previous = document.getElementById("previous");
 var isLastBtnNum = false;
-var operType = '';
-var values = {prev: null, now: null, total: null};
-var newValue;
+var equation = [];
+var values = null;
+var yourValue = null;
+var operator;
 
 
-function getNumbers(num) {
-    'use strict';
+
+
+function getNumbers(num) {                  //get a numbers
+    "use strict";
+    var result;
     if (isLastBtnNum) {
-        var result;
-        if (values.now) {
-            result = values.now + '' + num;
-            values.now = result;
-            current.value = result;
-        } else {
-            result = values.prev + '' + num;
-            values.prev = result;
-            current.value = result;
+        if (values) {
+            values = '' + values + num;
+            current.value = values;
         }
-        
     } else {
+        values = num;
         current.value = num;
-        if (values.prev) {
-            values.now = num;
-        } else if (newValue) {
-            values.now = num;
-        } else {
-            values.prev = num;
-        }
     }
     isLastBtnNum = true;
 }
-function getOperators(opr) {
-    'use strict';
-    if (values.now && (values.prev || newValue) && operType) {
-        calculate();
-        operType = opr;
+
+
+
+function getOperators(opr) {                // get the operator
+    "use strict";
+    if (yourValue) {
+        operator = opr;
+        equation.push(yourValue);
+        equation.push(operator);
+        yourValue = null;
+        current.value = '';
+        history1.innerHTML = equation.join("");
+        isLastBtnNum = false;
+    } else if (values) {
+        operator = opr;
+        equation.push(values);
+        equation.push(operator);
+        values = null;
+        current.value = '';
+        history1.innerHTML = equation.join("");
+        isLastBtnNum = false;
     } else {
-        operType = opr;
-    }
-        
-    isLastBtnNum = false;
-}
-function calculate() {
-    'use strict';
-    if (!values.now) {
+        operator = opr;
         return;
     }
-    if (values.now && values.prev && operType) {
-        
-        if (operType === "+") {
-            newValue = Number(values.prev) + Number(values.now);
-            current.value = newValue;
-            
-        }
-        if (operType === "-") {
-            newValue = Number(values.prev) - Number(values.now);
-            current.value = newValue;
-        
-        }
-        if (operType === "*") {
-            newValue = Number(values.prev) * Number(values.now);
-            current.value = newValue;
-        
-        }
-        if (operType === "/") {
-            newValue = Number(values.prev) / Number(values.now);
-            current.value = newValue;
-        }
-        values.prev = "";
-        
-    } else if (values.now && newValue && operType) {
-        
-        if (operType === "+") {
-            newValue = Number(newValue) + Number(values.now);
-            current.value = newValue;
-            
-        }
-        if (operType === "-") {
-            newValue = Number(newValue) - Number(values.now);
-            current.value = newValue;
-        
-        }
-        if (operType === "*") {
-            newValue = Number(newValue) * Number(values.now);
-            current.value = newValue;
-        
-        }
-        if (operType === "/") {
-            newValue = Number(newValue) / Number(values.now);
-            current.value = newValue;
-            
-        }
-        
-    }
-    
-    values.now = "";
 }
-isLastBtnNum = false;
 
-function clear1() {
+function calculate() {                      //evaluate the equation
     'use strict';
-    current.value = "";
-    values.prev = "";
-    values.now = "";
-}
-function back() {
-    'use strict';
-    current.value = current.value.substring(0, current.value.length - 1);
-    newValue = current.value;
-    values.prev = current.value;
-    values.now = current.value;
-}
-function toCurrencies(rate) {
-    'use strict';
-    if (values.prev) {
-        newValue = Number(values.prev) * rate;
-        current.value = newValue;
-        
-    } else {
-        newValue = Number(newValue) * rate;
-        current.value = newValue;
-       
+    equation.push(values);
+    equation.push(" =");
+    history1.innerHTML = equation.join("");
+    
+    function array() {
+        if (equation.includes(" * ")) {
+            var mul = equation.indexOf(" * "),
+                equal1 = Number(equation[mul - 1]) * Number(equation[mul + 1]);
+            equation.splice(mul - 1, 3, equal1);
+            array();
+        } else if (equation.includes(" / ")) {
+            var div = equation.indexOf(" / "),
+                equal2 = Number(equation[div - 1]) / Number(equation[div + 1]);
+            equation.splice(div - 1, 3, equal2);
+            array();
+        } else if (equation.includes(" + ")) {
+            var plus = equation.indexOf(" + "),
+                equal3 = Number(equation[plus - 1]) + Number(equation[plus + 1]);
+            equation.splice(plus - 1, 3, equal3);
+            array();
+        } else if (equation.includes(" - ")) {
+            var min = equation.indexOf(" - "),
+                equal4 = Number(equation[min - 1]) - Number(equation[min + 1]);
+            equation.splice(min - 1, 3, equal4);
+            array();
+        }
     }
-    values.prev = "";
+    array();
+    equation.pop();
+    current.value = equation;
+    values = current.value;
+    equation = [];
+    
+}
+
+
+
+
+
+function clear1() {                         //clear button
+    'use strict';
+    values = null;
+    current.value = "";
+    history1.innerHTML = "";
+    equation = [];
+    isLastBtnNum = false;
+
+}
+
+
+
+
+function back() {                           //backspace button
+    'use strict';
+    if (current.value.length <= 1) {
+        values = null;
+        current.value = "";
+        isLastBtnNum = false;
+    } else {
+        current.value = current.value.substring(0, current.value.length - 1);
+        values = values.substring(0, values.length - 1);
+    }
+}
+
+
+function toCurrencies(rate) {                           // function of currencies converter 
+    'use strict';
+    history1.innerHTML = values;
+    yourValue = Number(values) * rate;
+    current.value = yourValue;
+    isLastBtnNum = false;
+    
 }
 function fromCurrencies(rate) {
     'use strict';
-    if (values.prev) {
-        newValue = Number(values.prev) / rate;
-        current.value = newValue;
-        
-    } else {
-        newValue = Number(newValue) / rate;
-        current.value = newValue;
-       
-    }
-    values.prev = "";
+    history1.innerHTML = values;
+    yourValue = Number(values) / rate;
+    current.value = yourValue;
+    isLastBtnNum = false;
 }
-
